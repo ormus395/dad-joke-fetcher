5cfed58895df2adcef0e070bfb68f882f78743fb
ormus395 <ormus395@gmail.com>
2021-01-02 22:04:00 -0700

    Changed joke position

--- src/components/JokeContainer.js
+++ src/components/JokeContainer.js
@@ -2,10 +2,10 @@ import bbq from "../assets/bbq.svg";
 
 const JokeContainer = ({ joke, loaded }) => (
   <div className="joke-container">
+    <div className={`joke__content ${loaded ? "joke" : ""}`}>{joke}</div>
     <div className="bbq">
       <img src={bbq} alt="" />
     </div>
-    <div className={`joke__content ${loaded ? "joke" : ""}`}>{joke}</div>
   </div>
 );
 
